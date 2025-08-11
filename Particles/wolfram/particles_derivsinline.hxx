af42c2af5ec12d50ff67112630c50a35ef3717cb
Liwei Ji <jiliwei.phys@gmail.com>
2025-08-11 10:56:48 -0500

    Particles: Fix the component indexing in the stencil helpers

--- Particles/wolfram/particles_derivsinline.hxx
+++ Particles/wolfram/particles_derivsinline.hxx
@@ -14,8 +14,8 @@ using namespace Loop;
 template <int D, typename T>
 CCTK_DEVICE CCTK_HOST CCTK_ATTRIBUTE_ALWAYS_INLINE inline T
 fd_1_o2(amrex::Array4<T const> const &gf, int i, int j, int k, int comp, amrex::GpuArray<T, 3> const &dxi) {
-  const T m1 = gf(i + (D == 0 ? -1 : 0), j + (D == 1 ? -1 : 0), k + (D == 2 ? -1 : 0));
-  const T p1 = gf(i + (D == 0 ? 1 : 0), j + (D == 1 ? 1 : 0), k + (D == 2 ? 1 : 0));
+  const T m1 = gf(i + (D == 0 ? -1 : 0), j + (D == 1 ? -1 : 0), k + (D == 2 ? -1 : 0), comp);
+  const T p1 = gf(i + (D == 0 ? 1 : 0), j + (D == 1 ? 1 : 0), k + (D == 2 ? 1 : 0), comp);
   return
     (dxi[D]*(-m1 + p1))/2.;
 }
@@ -23,10 +23,10 @@ fd_1_o2(amrex::Array4<T const> const &gf, int i, int j, int k, int comp, amrex::
 template <int D, typename T>
 CCTK_DEVICE CCTK_HOST CCTK_ATTRIBUTE_ALWAYS_INLINE inline T
 fd_1_o4(amrex::Array4<T const> const &gf, int i, int j, int k, int comp, amrex::GpuArray<T, 3> const &dxi) {
-  const T m2 = gf(i + (D == 0 ? -2 : 0), j + (D == 1 ? -2 : 0), k + (D == 2 ? -2 : 0));
-  const T m1 = gf(i + (D == 0 ? -1 : 0), j + (D == 1 ? -1 : 0), k + (D == 2 ? -1 : 0));
-  const T p1 = gf(i + (D == 0 ? 1 : 0), j + (D == 1 ? 1 : 0), k + (D == 2 ? 1 : 0));
-  const T p2 = gf(i + (D == 0 ? 2 : 0), j + (D == 1 ? 2 : 0), k + (D == 2 ? 2 : 0));
+  const T m2 = gf(i + (D == 0 ? -2 : 0), j + (D == 1 ? -2 : 0), k + (D == 2 ? -2 : 0), comp);
+  const T m1 = gf(i + (D == 0 ? -1 : 0), j + (D == 1 ? -1 : 0), k + (D == 2 ? -1 : 0), comp);
+  const T p1 = gf(i + (D == 0 ? 1 : 0), j + (D == 1 ? 1 : 0), k + (D == 2 ? 1 : 0), comp);
+  const T p2 = gf(i + (D == 0 ? 2 : 0), j + (D == 1 ? 2 : 0), k + (D == 2 ? 2 : 0), comp);
   return
     (dxi[D]*(-8*m1 + m2 + 8*p1 - p2))/12.;
 }
@@ -34,12 +34,12 @@ fd_1_o4(amrex::Array4<T const> const &gf, int i, int j, int k, int comp, amrex::
 template <int D, typename T>
 CCTK_DEVICE CCTK_HOST CCTK_ATTRIBUTE_ALWAYS_INLINE inline T
 fd_1_o6(amrex::Array4<T const> const &gf, int i, int j, int k, int comp, amrex::GpuArray<T, 3> const &dxi) {
-  const T m3 = gf(i + (D == 0 ? -3 : 0), j + (D == 1 ? -3 : 0), k + (D == 2 ? -3 : 0));
-  const T m2 = gf(i + (D == 0 ? -2 : 0), j + (D == 1 ? -2 : 0), k + (D == 2 ? -2 : 0));
-  const T m1 = gf(i + (D == 0 ? -1 : 0), j + (D == 1 ? -1 : 0), k + (D == 2 ? -1 : 0));
-  const T p1 = gf(i + (D == 0 ? 1 : 0), j + (D == 1 ? 1 : 0), k + (D == 2 ? 1 : 0));
-  const T p2 = gf(i + (D == 0 ? 2 : 0), j + (D == 1 ? 2 : 0), k + (D == 2 ? 2 : 0));
-  const T p3 = gf(i + (D == 0 ? 3 : 0), j + (D == 1 ? 3 : 0), k + (D == 2 ? 3 : 0));
+  const T m3 = gf(i + (D == 0 ? -3 : 0), j + (D == 1 ? -3 : 0), k + (D == 2 ? -3 : 0), comp);
+  const T m2 = gf(i + (D == 0 ? -2 : 0), j + (D == 1 ? -2 : 0), k + (D == 2 ? -2 : 0), comp);
+  const T m1 = gf(i + (D == 0 ? -1 : 0), j + (D == 1 ? -1 : 0), k + (D == 2 ? -1 : 0), comp);
+  const T p1 = gf(i + (D == 0 ? 1 : 0), j + (D == 1 ? 1 : 0), k + (D == 2 ? 1 : 0), comp);
+  const T p2 = gf(i + (D == 0 ? 2 : 0), j + (D == 1 ? 2 : 0), k + (D == 2 ? 2 : 0), comp);
+  const T p3 = gf(i + (D == 0 ? 3 : 0), j + (D == 1 ? 3 : 0), k + (D == 2 ? 3 : 0), comp);
   return
     (dxi[D]*(-45*m1 + 9*m2 - m3 + 45*p1 - 9*p2 + p3))/60.;
 }
@@ -47,14 +47,14 @@ fd_1_o6(amrex::Array4<T const> const &gf, int i, int j, int k, int comp, amrex::
 template <int D, typename T>
 CCTK_DEVICE CCTK_HOST CCTK_ATTRIBUTE_ALWAYS_INLINE inline T
 fd_1_o8(amrex::Array4<T const> const &gf, int i, int j, int k, int comp, amrex::GpuArray<T, 3> const &dxi) {
-  const T m4 = gf(i + (D == 0 ? -4 : 0), j + (D == 1 ? -4 : 0), k + (D == 2 ? -4 : 0));
-  const T m3 = gf(i + (D == 0 ? -3 : 0), j + (D == 1 ? -3 : 0), k + (D == 2 ? -3 : 0));
-  const T m2 = gf(i + (D == 0 ? -2 : 0), j + (D == 1 ? -2 : 0), k + (D == 2 ? -2 : 0));
-  const T m1 = gf(i + (D == 0 ? -1 : 0), j + (D == 1 ? -1 : 0), k + (D == 2 ? -1 : 0));
-  const T p1 = gf(i + (D == 0 ? 1 : 0), j + (D == 1 ? 1 : 0), k + (D == 2 ? 1 : 0));
-  const T p2 = gf(i + (D == 0 ? 2 : 0), j + (D == 1 ? 2 : 0), k + (D == 2 ? 2 : 0));
-  const T p3 = gf(i + (D == 0 ? 3 : 0), j + (D == 1 ? 3 : 0), k + (D == 2 ? 3 : 0));
-  const T p4 = gf(i + (D == 0 ? 4 : 0), j + (D == 1 ? 4 : 0), k + (D == 2 ? 4 : 0));
+  const T m4 = gf(i + (D == 0 ? -4 : 0), j + (D == 1 ? -4 : 0), k + (D == 2 ? -4 : 0), comp);
+  const T m3 = gf(i + (D == 0 ? -3 : 0), j + (D == 1 ? -3 : 0), k + (D == 2 ? -3 : 0), comp);
+  const T m2 = gf(i + (D == 0 ? -2 : 0), j + (D == 1 ? -2 : 0), k + (D == 2 ? -2 : 0), comp);
+  const T m1 = gf(i + (D == 0 ? -1 : 0), j + (D == 1 ? -1 : 0), k + (D == 2 ? -1 : 0), comp);
+  const T p1 = gf(i + (D == 0 ? 1 : 0), j + (D == 1 ? 1 : 0), k + (D == 2 ? 1 : 0), comp);
+  const T p2 = gf(i + (D == 0 ? 2 : 0), j + (D == 1 ? 2 : 0), k + (D == 2 ? 2 : 0), comp);
+  const T p3 = gf(i + (D == 0 ? 3 : 0), j + (D == 1 ? 3 : 0), k + (D == 2 ? 3 : 0), comp);
+  const T p4 = gf(i + (D == 0 ? 4 : 0), j + (D == 1 ? 4 : 0), k + (D == 2 ? 4 : 0), comp);
   return
     (dxi[D]*(-672*m1 + 168*m2 - 32*m3 + 3*m4 + 672*p1 - 168*p2 + 32*p3 - 3*p4))/840.;
 }
