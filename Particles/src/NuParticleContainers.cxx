ddf18fadc12a2143b8a557e23c2d866027034fe3
Liwei Ji <jiliwei.phys@gmail.com>
2025-08-11 14:06:16 -0500

    Particles: remove restrict for reference

--- Particles/src/NuParticleContainers.cxx
+++ Particles/src/NuParticleContainers.cxx
@@ -178,7 +178,7 @@ extern "C" void NuParticleContainers_Setup(CCTK_ARGUMENTS) {
   DECLARE_CCTK_PARAMETERS;
 
   for (int patch = 0; patch < CarpetX::ghext->num_patches(); ++patch) {
-    const auto &restrict patchdata = CarpetX::ghext->patchdata.at(patch);
+    const auto &patchdata = CarpetX::ghext->patchdata.at(patch);
     g_nupcs.emplace_back(
         std::make_unique<NuParticleContainer>(patchdata.amrcore.get()));
   } // for patch
